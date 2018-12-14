7a16df88d5cf463c742d4cd1419ea4015f5957b0
eAzTea <45879371+eAzTea@users.noreply.github.com>
2018-12-15 00:12:06 +0100

    Update apekzbotv1.py

--- apekzbotv1.py
+++ apekzbotv1.py
@@ -27,3 +27,11 @@ async def on_message(message):
     if message.content == '$help':
         await client.send_message(message.channel,'Wenn du Neu bist melde dich zuerst bei einen Operator!')
 client.run('NTIyNzg5Njc5ODcxODE5Nzg2.DvQFhw.INWP8K064raDypBIw5lw50ztT8M')
+    if message.content == '$apekzlogo':
+        em = discord.Embed(description='')
+        em.set_image(url='https://cdn.discordapp.com/attachments/520952616843214858/523275401829810186/Apexz_Logo.png')
+        await client.send_message(message.channel, embed=em)
+    if message.content == '$apekzavatar':
+        em = discord.Embed(description='')
+        em.set_image(url='https://cdn.discordapp.com/attachments/520952616843214858/523275442334203915/Apexz_Avatar.png')
+        await client.send_message(message.channel, embed=em)
