a9fecb2ac71ab3213d5b202f2a780bdd97953a7c
eAzTea <45879371+eAzTea@users.noreply.github.com>
2019-11-16 21:52:19 +0100

    Update apekzbotv1.py

--- apekzbotv1.py
+++ apekzbotv1.py
@@ -11,25 +11,21 @@ Client = discord.client
 client = commands.Bot(command_prefix = '!')
 Clientdiscord = discord.Client()
 
-
 @client.event
-async def on_member_join(member):
-    await client.change_presence(game=Game(name='Rainbow Six Siege'))
-    await client.send_message(member, 'Willkommen bei Apekz, melde dich zuerst unter den Channel hiermelden. Darauf wird Dir ein Operator alles erklären! Operator: Lunar_Berry | Leitung: eAzTeA & Karuukan')
-    print('Sent message to ' + member.name)
 async def on_ready():
-    await client.change_presence(game=Game(name='      '))
-    print('Ready, Freddy') 
+    print('Bot online!')
 
+@client.command()
+async def status(ctx):
+    await ctx.author.send('Status: https://teatimemodding.wixsite.com/modmenus/status')
+@client.command()
+async def sc(ctx):
+    await ctx.author.send('Add me on Social Club: https://boost.ink/-t1z9')
+@client.command()
+async def website(ctx):
+    await ctx.author.send('Modmenu Website: https://teatimemodding.wixsite.com/modmenus')
+@client.command()
+async def com(ctx):
+    await ctx.author.send('``` BOT COMMANDS: !sc (Send you the Link for my Socialclubname) | !status (Send you the Link to the Status from the Modmenus) | !website(Send you the Link for my Website) ```')
+client.run('NjQ1MzIzMzI3NDM0NTIyNjU0.XdBcUg.uj2XQuDd2OurJuKeCyuZebZd2TY')
 
-@client.event
-async def on_message(message):
-    if message.content == '$help':
-        await client.send_message(message.channel,'Commands: $avatar ; $logo ; $binneu')
-    if message.content == '$logo':
-        await client.send_message(message.channel,'https://cdn.discordapp.com/attachments/520952616843214858/523275401829810186/Apexz_Logo.png')
-    if message.content == '$avatar':
-        await client.send_message(message.channel,'https://cdn.discordapp.com/attachments/520952616843214858/523275442334203915/Apexz_Avatar.png')
-    if message.content == '$binneu':
-        await client.send_message(message.channel,'Wenn du Neu bist melde dich zuerst bei einen Operator!')
-client.run('NTIyNzg5Njc5ODcxODE5Nzg2.DvZ6YA.NznqWz7Gm2ce2FWpD-hXu_7HN_s')
